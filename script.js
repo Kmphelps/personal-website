4eeb87439c048afacb3c93eda9e662b61c478890
Kayla Phelps <kmphelps822@gmail.com>
2021-07-11 12:21:44 -0700

    added image and event listener

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 document.querySelector('body').appendChild(h2);
+
+
+function addingEventListener() {
+    
+    const input = document.getElementById('flowers');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert); 
+
+}
+
+addingEventListener();
